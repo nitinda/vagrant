cc78e9c7ed401d2260a08f0a73cbc25ce4c3eff6
nitin-das <nitin.das@thomson.co.uk>
2018-10-24 09:55:09 +0100

    Added insatll step for Install vboxadd-service and vboxadd

--- packer/centos7/http/scripts/base.sh
+++ packer/centos7/http/scripts/base.sh
@@ -12,6 +12,17 @@ sudo echo "vagrant ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/vagrant
 sudo echo "Defaults:vagrant !requiretty" >> /etc/sudoers.d/vagrant
 sudo chmod 0440 /etc/sudoers.d/vagrant
 
+# Install vboxadd-service and vboxadd
+
+sudo yum -y install vboxadd-service vboxadd
+
+sudo systemctl enable vboxadd-service.service
+sudo systemctl enable vboxadd.service
+
+sudo systemctl start vboxadd.service
+sudo systemctl start vboxadd-service.service
+
+
 # Rest yum operations
 sudo yum -y update
 sudo yum -y upgrade
